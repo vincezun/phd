358c83412056078312920ede1a5fe1e65b66f6e3
vincezun <vncnzo@yahoo.com>
2019-06-11 04:07:02 +0800

    Added stuff

---
+++ src/components/get-in-touch.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import '../styles/get-in-touch.scss';
+
+const GetInTouch = () => (
+  <section className='stripe-tile-background'>
+    <div className='get-in-touch w-container'>
+      <div>
+        <h2 className='title'>
+          Cant find what your looking for? We can source most equipment.
+        </h2>
+        <Link to='/contact' className='get-in-touch-link' role='button'>
+          Get in touch
+        </Link>
+      </div>
+      <div>
+        <p className='description'>
+          With over 20 years experience the PhD team has met every demanding
+          situation the industry can throw up.  We pride ourselves on our
+          customers and the loyalty they have continuously shown through the
+          years.  We are a nationwide plant supplier offering quick solutions to
+          demanding situations and understand the importance of quick turn
+          around solutions.
+        </p>
+      </div>
+    </div>
+  </section>
+);
+
+export default GetInTouch;
---
+++ src/components/testimonial.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import '../styles/testimonial.scss';
+
+const Testimonial = () => (
+  <section className='yellow-background'>
+    <div className='testimonial w-container'>
+      <p className='quote'>
+        “Without doubt Plant Hire Division have lorum ipsum dorum montes
+        nascetur sed ridiccis”
+      </p>
+      <p className='client'>A. Customer, SSI Schaffer</p>
+    </div>
+  </section>
+);
+
+export default Testimonial;
