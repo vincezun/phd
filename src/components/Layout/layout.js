174405809352b3402eb762647f55de26b76ec17d
vincezun <vncnzo@yahoo.com>
2019-06-12 04:18:05 +0800

    Initialized aos

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -6,6 +6,10 @@ import Footer from './footer';
 
 import '../../styles/layout.scss';
 
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
+
 const Layout = ({ children }) => (
   <StaticQuery
     query={graphql`
