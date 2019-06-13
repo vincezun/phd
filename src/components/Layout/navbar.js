5e2bde6520b9ca4fbe32912a855f2f1aaf684fec
vincezun <vncnzo@yahoo.com>
2019-06-13 16:20:58 +0800

    Changed close button and arrow down src

--- src/components/Layout/navbar.js
+++ src/components/Layout/navbar.js
@@ -8,8 +8,8 @@ import '../../styles/navbar.scss';
 
 import phdLogo from '../../images/phd-logo.png';
 import menuBtn from '../../images/menu-btn.png';
-import closeBtn from '../../images/close-btn.png';
-import arrowDownBlack from '../../images/arrow-down-black.png';
+import closeBtnWhite from '../../images/close-btn-white.png';
+import closeBtnBlack from '../../images/close-btn-black.png';
 
 import CallBtn from '../Button/button-1';
 
@@ -23,14 +23,33 @@ class Navbar extends Component {
     };
   }
 
-  openModal = () => {
-    this.setState({ isOpen: true });
+  handleResize = () => {
+    var width = window.innerWidth;
+    var maxWidth = 992;
+    var overlay = document.getElementById('nav-overlay');
 
-    this.hideOverlay();
+    if (width >= maxWidth) {
+      document.body.classList.remove('scroll-disabled');
+      overlay.classList.add('transition-disabled');
+    } else if (overlay.classList.contains('show-overlay')) {
+      document.body.classList.add('scroll-disabled');
+      overlay.classList.remove('transition-disabled');
+    } else if (width < maxWidth) {
+      overlay.classList.remove('transition-disabled');
+    }
   };
 
-  afterOpenModal = () => {
-    this.overlay.style.backgroundColor = 'yellow';
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize);
+  }
+
+  openModal = () => {
+    this.setState({ isOpen: true });
+    this.hideNavOverlay();
   };
 
   closeModal = () => {
@@ -43,7 +62,7 @@ class Navbar extends Component {
     document.body.classList.add('scroll-disabled');
   };
 
-  hideOverlay = () => {
+  hideNavOverlay = () => {
     var overlay = document.getElementById('nav-overlay');
     overlay.classList.remove('show-overlay');
     document.body.classList.remove('scroll-disabled');
@@ -83,6 +102,7 @@ class Navbar extends Component {
             data-aos-easing='ease'
             data-aos-duration='700'
             data-aos-delay='100'
+            data-aos-once='true'
           >
             <Link to='/'>
               <img
@@ -93,208 +113,221 @@ class Navbar extends Component {
             </Link>
           </div>
           <ul className='nav-overlay' id='nav-overlay' role='tree'>
-            <img
-              onClick={this.hideOverlay}
-              src={closeBtn}
-              alt='Close button'
-              className='close-btn'
-              role='button'
-            />
-            <li
-              onClick={this.toggleDropdown}
-              className='dropdown-toggle'
-              id='dropdown-toggle'
-              role='presentation'
+            <div
+              className='w-nav-overlay'
+              data-aos='fade'
+              data-aos-easing='ease'
+              data-aos-duration='700'
+              data-aos-delay='100'
+              data-aos-once='true'
             >
-              <div className='dropdown-menu'>
-                <p>Services</p>
-                <img
-                  src={arrowDownBlack}
-                  alt='Arrow down black'
-                  className='arrow-down-black'
-                />
-              </div>
-              <ul className='dropdown-link' role='tree'>
-                <li role='presentation'>
-                  <Link
-                    to='/plant-hire'
-                    role='treeitem'
-                    activeClassName='active-link'
-                  >
-                    Plant Hire
-                  </Link>
-                </li>
-                <li role='presentation'>
-                  <Link
-                    to='/working-platform'
-                    role='treeitem'
-                    activeClassName='active-link'
-                  >
-                    Working Plantform
-                  </Link>
-                </li>
-                <li role='presentation'>
-                  <Link
-                    to='/specialities'
-                    role='treeitem'
-                    activeClassName='active-link'
-                  >
-                    Specialities
-                  </Link>
-                </li>
-                <li role='presentation'>
-                  <Link
-                    to='/tool-hire'
-                    role='treeitem'
-                    activeClassName='active-link'
-                  >
-                    Tool Hire
-                  </Link>
-                </li>
-              </ul>
-            </li>
-            <li role='presentation'>
-              <Link
-                to='/case-studies'
-                role='treeitem'
-                activeClassName='active-link'
+              <img
+                onClick={this.hideNavOverlay}
+                src={closeBtnBlack}
+                alt='Close button'
+                className='close-btn-black'
+                role='button'
+              />
+              <li
+                onClick={this.toggleDropdown}
+                className='dropdown-toggle'
+                id='dropdown-toggle'
+                role='presentation'
               >
-                Case Studies
-              </Link>
-            </li>
-            <li role='presentation'>
-              <Link
-                to='/resources'
-                role='treeitem'
-                activeClassName='active-link'
-              >
-                Resources
-              </Link>
-            </li>
-            <li role='presentation'>
-              <Link
-                to='/training'
-                role='treeitem'
-                activeClassName='active-link'
-              >
-                Training
-              </Link>
-            </li>
-            <li role='presentation'>
-              <Link to='/contact' role='treeitem' activeClassName='active-link'>
-                Contact
-              </Link>
-            </li>
-            <li role='presentation'>
-              <p
-                onClick={this.openModal}
-                className='request-callback-link'
-                role='treeitem'
-              >
-                Request Callback
-              </p>
-              <Modal
-                isOpen={this.state.isOpen}
-                contentLabel='Request Callback'
-                onRequestClose={this.closeModal}
-                className='modal-content'
-                overlayClassName='modal-overlay'
-              >
-                <img
-                  onClick={this.closeModal}
-                  src={closeBtn}
-                  alt='Close button'
-                  className='close-modal-btn'
-                  role='button'
-                />
-
-                <Formik
-                  initialValues={{
-                    fullName: '',
-                    phoneNumber: ''
-                  }}
-                  validationSchema={Yup.object().shape({
-                    fullName: Yup.string()
-                      .required('This field is required.')
-                      .max(80, 'Please enter no more than 80 characters'),
-                    phoneNumber: Yup.string()
-                      .required('This field is required.')
-                      .matches(phoneRegex, {
-                        excludeEmptyString: true,
-                        message: 'Invalid phone number'
-                      })
-                      .max(50, 'Please enter no more than 50 characters')
-                  })}
-                  onSubmit={this.onSubmit}
-                  render={({
-                    errors,
-                    values,
-                    touched,
-                    handleChange,
-                    handleBlur,
-                    handleSubmit,
-                    isSubmitting
-                  }) => (
-                    <div className='callback-form'>
-                      <p className='callback-heading'>Request Callback</p>
-                      <p className='callback-description'>
-                        Add your details below and we'll get back to you soon as
-                        possible.
-                      </p>
-                      <div className='w-full-name' id='w-full-name'>
-                        <label htmlFor='fullName'>Full Name</label>
+                <div className='dropdown-menu'>
+                  <p>Services</p>
+                  <div
+                    aria-label='Arrow down black'
+                    role='button'
+                    className='arrow-down-black'
+                  />
+                </div>
+                <ul className='dropdown-link' role='tree'>
+                  <li role='presentation'>
+                    <Link
+                      to='/plant-hire'
+                      role='treeitem'
+                      activeClassName='active-link'
+                    >
+                      Plant Hire
+                    </Link>
+                  </li>
+                  <li role='presentation'>
+                    <Link
+                      to='/working-platform'
+                      role='treeitem'
+                      activeClassName='active-link'
+                    >
+                      Working Plantform
+                    </Link>
+                  </li>
+                  <li role='presentation'>
+                    <Link
+                      to='/specialities'
+                      role='treeitem'
+                      activeClassName='active-link'
+                    >
+                      Specialities
+                    </Link>
+                  </li>
+                  <li role='presentation'>
+                    <Link
+                      to='/tool-hire'
+                      role='treeitem'
+                      activeClassName='active-link'
+                    >
+                      Tool Hire
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+              <li role='presentation'>
+                <Link
+                  to='/case-studies'
+                  role='treeitem'
+                  activeClassName='active-link'
+                >
+                  Case Studies
+                </Link>
+              </li>
+              <li role='presentation'>
+                <Link
+                  to='/resources'
+                  role='treeitem'
+                  activeClassName='active-link'
+                >
+                  Resources
+                </Link>
+              </li>
+              <li role='presentation'>
+                <Link
+                  to='/training'
+                  role='treeitem'
+                  activeClassName='active-link'
+                >
+                  Training
+                </Link>
+              </li>
+              <li role='presentation'>
+                <Link
+                  to='/contact'
+                  role='treeitem'
+                  activeClassName='active-link'
+                >
+                  Contact
+                </Link>
+              </li>
+              <li role='presentation'>
+                <p
+                  onClick={this.openModal}
+                  className='request-callback-link'
+                  role='treeitem'
+                >
+                  Request Callback
+                </p>
+                <Modal
+                  closeTimeoutMS={450}
+                  isOpen={this.state.isOpen}
+                  contentLabel='Request Callback'
+                  onRequestClose={this.closeModal}
+                  className='modal-content'
+                  overlayClassName='modal-overlay'
+                >
+                  <img
+                    onClick={this.closeModal}
+                    src={closeBtnWhite}
+                    alt='Close button'
+                    className='close-modal-btn'
+                    role='button'
+                  />
+                  <Formik
+                    initialValues={{
+                      fullName: '',
+                      phoneNumber: ''
+                    }}
+                    validationSchema={Yup.object().shape({
+                      fullName: Yup.string()
+                        .required('This field is required.')
+                        .max(80, 'Please enter no more than 80 characters'),
+                      phoneNumber: Yup.string()
+                        .required('This field is required.')
+                        .matches(phoneRegex, {
+                          excludeEmptyString: true,
+                          message: 'Invalid phone number'
+                        })
+                        .max(50, 'Please enter no more than 50 characters')
+                    })}
+                    onSubmit={this.onSubmit}
+                    render={({
+                      errors,
+                      values,
+                      touched,
+                      handleChange,
+                      handleBlur,
+                      handleSubmit,
+                      isSubmitting
+                    }) => (
+                      <div className='callback-form'>
+                        <p className='callback-heading'>Request Callback</p>
+                        <p className='callback-description'>
+                          Add your details below and we'll get back to you soon
+                          as possible.
+                        </p>
+                        <div className='w-full-name' id='w-full-name'>
+                          <label htmlFor='fullName'>Full Name</label>
+                          <Field
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.fullName}
+                            type='text'
+                            name='fullName'
+                            className={
+                              errors.fullName && touched.fullName
+                                ? 'full-name-field err'
+                                : 'full-name-field'
+                            }
+                          />
+                          <ErrorMessage name='fullName'>
+                            {msg => <div className='err-msg'>{msg}</div>}
+                          </ErrorMessage>
+                        </div>
+                        <div className='w-phone-number' id='w-phone-number'>
+                          <label htmlFor='phoneNumber'>Phone Number</label>
+                          <Field
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.phoneNumber}
+                            type='text'
+                            name='phoneNumber'
+                            className={
+                              errors.phoneNumber && touched.phoneNumber
+                                ? 'number-field err'
+                                : 'number-field'
+                            }
+                          />
+                          <ErrorMessage name='phoneNumber'>
+                            {msg => <div className='err-msg'>{msg}</div>}
+                          </ErrorMessage>
+                        </div>
                         <Field
-                          onChange={handleChange}
-                          onBlur={handleBlur}
-                          value={values.fullName}
-                          type='text'
-                          name='fullName'
-                          className={
-                            errors.fullName && touched.fullName
-                              ? 'full-name-field err'
-                              : 'full-name-field'
-                          }
+                          type='submit'
+                          disabled={isSubmitting}
+                          onClick={handleSubmit}
+                          className='submit-form-btn'
+                          id='submit-form-btn'
+                          value='Submit Form'
+                          aria-label='Submit Form'
+                          role='button'
                         />
-                        <ErrorMessage name='fullName'>
-                          {msg => <div className='err-msg'>{msg}</div>}
-                        </ErrorMessage>
+                        <div className='success-msg' id='success-msg'>
+                          <p>Thank you! Your submission has been received!</p>
+                        </div>
                       </div>
-                      <div className='w-phone-number' id='w-phone-number'>
-                        <label htmlFor='phoneNumber'>Phone Number</label>
-                        <Field
-                          onChange={handleChange}
-                          onBlur={handleBlur}
-                          value={values.phoneNumber}
-                          type='text'
-                          name='phoneNumber'
-                          className={
-                            errors.phoneNumber && touched.phoneNumber
-                              ? 'number-field err'
-                              : 'number-field'
-                          }
-                        />
-                        <ErrorMessage name='phoneNumber'>
-                          {msg => <div className='err-msg'>{msg}</div>}
-                        </ErrorMessage>
-                      </div>
-                      <Field
-                        type='submit'
-                        disabled={isSubmitting}
-                        onClick={handleSubmit}
-                        className='submit-form-btn'
-                        id='submit-form-btn'
-                        value='Submit Form'
-                        aria-label='Submit Form'
-                        role='button'
-                      />
-                      <div className='success-msg' id='success-msg'>
-                        <p>Thank you! Your submission has been received!</p>
-                      </div>
-                    </div>
-                  )}
-                />
-              </Modal>
-            </li>
+                    )}
+                  />
+                </Modal>
+              </li>
+            </div>
           </ul>
         </div>
         <div className='menu-section w-container'>
