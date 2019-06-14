704470de01d7120c3f791fd89115024c2f6f6665
vincezun <vncnzo@yahoo.com>
2019-06-14 21:09:13 +0800

    Moved aos init and added refreshHard

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import { StaticQuery, graphql } from 'gatsby';
 
 import Navbar from './navbar';
@@ -8,29 +8,38 @@ import '../../styles/layout.scss';
 
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-AOS.init();
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
+class Layout extends Component {
+  componentDidMount() {
+    AOS.init();
+    AOS.refreshHard();
+  }
+
+  render() {
+    const children = this.props.children;
+    return (
+      <StaticQuery
+        query={graphql`
+          query SiteTitleQuery {
+            site {
+              siteMetadata {
+                title
+              }
+            }
           }
-        }
-      }
-    `}
-    render={() => (
-      <Fragment>
-        <Navbar />
-        <div>
-          <main role='main'>{children}</main>
-        </div>
-        <Footer />
-      </Fragment>
-    )}
-  />
-);
+        `}
+        render={() => (
+          <Fragment>
+            <Navbar />
+            <div>
+              <main role='main'>{children}</main>
+            </div>
+            <Footer />
+          </Fragment>
+        )}
+      />
+    );
+  }
+}
 
 export default Layout;
