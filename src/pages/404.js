9b28bac62bf9b009d6e69f0475ca36aa8d99ffd2
vincezun <vncnzo@yahoo.com>
2019-06-27 14:36:07 +0800

    Added stuff

---
+++ src/pages/404.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import '../styles/404.scss';
+
+import GoBackToHomePage from '../components/Button/button-4';
+
+const PageNotFound = () => (
+  <div className='page-not-found-background'>
+    <h1 className='title'>404</h1>
+    <p className='sub-title'>PAGE NOT FOUND</p>
+    <p className='description'>
+      We looked everywhere for this page. <br />
+      Are you sure the website URL is correct? <br />
+      Get in touch with the site owner.
+    </p>
+    <GoBackToHomePage />
+  </div>
+);
+
+export default PageNotFound;
