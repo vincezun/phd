a877e417d925e3591d3e46a8e899d1c10a409061
Vince Zuño <vincezun@yahoo.com>
2019-07-13 13:19:46 +0800

    Added aos

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -17,81 +17,75 @@ import regular from '../../fonts/gilroy-regular-webfont.woff2';
 import semibold from '../../fonts/gilroy-semibold-webfont.woff2';
 import semibolditalic from '../../fonts/gilroy-semibolditalic-webfont.woff2';
 
-const Layout = ({ children }) => {
-  return (
-    <StaticQuery
-      query={graphql`
-        query SiteTitleQuery {
-          site {
-            siteMetadata {
-              title
+class Layout extends Component {
+  componentDidMount() {
+    AOS.init();
+    AOS.refreshHard();
+  }
+
+  render() {
+    const children = this.props.children;
+    return (
+      <StaticQuery
+        query={graphql`
+          query SiteTitleQuery {
+            site {
+              siteMetadata {
+                title
+              }
             }
           }
-        }
-      `}
-      render={() => (
-        <Fragment>
-          <Helmet>
-            <link
-              rel="preload"
-              as="font"
-              href={light}
-              type="font/woff2"
-              crossOrigin="anonymous"
-            />
-            <link
-              rel="preload"
-              as="font"
-              href={regular}
-              type="font/woff2"
-              crossOrigin="anonymous"
-            />
-            <link
-              rel="preload"
-              as="font"
-              href={semibold}
-              type="font/woff2"
-              crossOrigin="anonymous"
-            />
-            <link
-              rel="preload"
-              as="font"
-              href={semibolditalic}
-              type="font/woff2"
-              crossOrigin="anonymous"
-            />
-            <link
-              rel="preload"
-              as="font"
-              href={bold}
-              type="font/woff2"
-              crossOrigin="anonymous"
-            />
-          </Helmet>
-          <Navbar />
-          <div>
-            <main role="main">{children}</main>
-          </div>
-          <Footer />
-        </Fragment>
-      )}
-    />
-  );
-};
-
-// class Layout extends Component {
-//   // componentDidMount() {
-//   //   AOS.init();
-//   //   AOS.refreshHard();
-//   // }
-
-//   render() {
-//     const children = this.props.children;
-//     return (
-
-//     );
-//   }
-// }
+        `}
+        render={() => (
+          <Fragment>
+            <Helmet>
+              <link
+                rel="preload"
+                as="font"
+                href={light}
+                type="font/woff2"
+                crossOrigin="anonymous"
+              />
+              <link
+                rel="preload"
+                as="font"
+                href={regular}
+                type="font/woff2"
+                crossOrigin="anonymous"
+              />
+              <link
+                rel="preload"
+                as="font"
+                href={semibold}
+                type="font/woff2"
+                crossOrigin="anonymous"
+              />
+              <link
+                rel="preload"
+                as="font"
+                href={semibolditalic}
+                type="font/woff2"
+                crossOrigin="anonymous"
+              />
+              <link
+                rel="preload"
+                as="font"
+                href={bold}
+                type="font/woff2"
+                crossOrigin="anonymous"
+              />
+            </Helmet>
+            <Navbar />
+            <div>
+              <main role="main">{children}</main>
+            </div>
+            <Footer />
+          </Fragment>
+        )}
+      />
+    );
+  }
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired
